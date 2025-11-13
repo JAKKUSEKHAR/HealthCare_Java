2ae91d66b9604a5d3c3eca6cf9a4a26ee015fe57
JAKKU SEKHAR <jakkusekhar22@gmail.com>
2025-11-13 15:32:03 +0530

    Added methods inside Hospitals Class

--- src/com/healthcare/Hospitals.java
+++ src/com/healthcare/Hospitals.java
@@ -2,4 +2,13 @@ package com.healthcare;
 
 public class Hospitals {
 
+	void swims() {
+		System.out.println("This is Swims Hospital");
+	}
+	void kgh() {
+		System.out.println("This is KGH Hospital");
+	}
+	void dolphin() {
+		System.out.println("This is Dolphin");
+	}
 }
