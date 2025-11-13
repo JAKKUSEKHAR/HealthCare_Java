5589578cb8f1c20fd8e2fae32d66cb8b7247963a
kondu <kondu@SURYA>
2025-11-13 08:17:25 +0530

    added doctor class

--- src/com/healthcare/Patient.java
+++ src/com/healthcare/Patient.java
@@ -4,6 +4,7 @@ public class Patient {
 
 	void disease()
 	{
-	System.out.println("Suffering From late night Sleep");	
+	System.out.println("Suffering From late night Sleep");
+	System.out.println("first pull request");
 	}
 }
